0b6a7ac56980f9f987da895f9e16ef13631f6c48
Yurim Yang <yang980329@likelion.org>
2022-01-31 02:30:58 +0900

    220131 Build :: 'userRoute.js' 'userService.js' 수정
    userRoute.js-라우트 추가, userService.js-index변경

--- COLLECTION/src/app/User/userRoute.js
+++ COLLECTION/src/app/User/userRoute.js
@@ -5,23 +5,32 @@ module.exports = function(app){
     // 0. 테스트 API
     // app.get('/app/test', user.getTest)
 
-    // 1. 유저 생성 (회원가입) API
+    // 1. 회원가입API
     app.post('/app/users/register', user.postUsers);
 
-    // 2. 유저 조회 API (+ 검색)
-    app.get('/app/users',user.getUsers); 
+    // 2. 중복 ID 확인 
+    app.get('/app/users/duplicate-id',user.duplicate_ID);
 
-    // 3. 특정 유저 조회 API
-    app.get('/app/users/:userId', user.getUserById);
+    // 3. 중복 닉네임 확인
+    app.get('/app/users/duplicate-nickname',user.duplicate_nickname);
+
+    // 4. 로그인 
+    // app.post('/app/user/login',user.login);
+
+    // // 2. 유저 조회 API (+ 검색)
+    // app.get('/app/users',user.getUsers); 
+
+    // // 3. 특정 유저 조회 API
+    // app.get('/app/users/:userId', user.getUserById);
 
 
-    // 아래 부분은 7주차에서 다룸.
-    // TODO: After 로그인 인증 방법 (JWT)
-    // 로그인 하기 API (JWT 생성)
-    app.post('/app/login', user.login);
+    // // 아래 부분은 7주차에서 다룸.
+    // // TODO: After 로그인 인증 방법 (JWT)
+    // // 로그인 하기 API (JWT 생성)
+    // app.post('/app/login', user.login);
 
-    // 회원 정보 수정 API (JWT 검증 및 Validation - 메소드 체이닝 방식으로 jwtMiddleware 사용)
-    app.patch('/app/users/:userId', jwtMiddleware, user.patchUsers)
+    // // 회원 정보 수정 API (JWT 검증 및 Validation - 메소드 체이닝 방식으로 jwtMiddleware 사용)
+    // app.patch('/app/users/:userId', jwtMiddleware, user.patchUsers)
 
 
 
