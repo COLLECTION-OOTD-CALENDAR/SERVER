1ea61f71ef6acc39e39a3b313da39ceaf1120127
CSH <sayong4424@gmail.com>
2022-02-05 06:31:38 +0900

    220205 Style :: 출력에 쓰이는 key name 변경
    key name을 클라이언트에서 보다 더 쉽게 변경 및 console log 추가

--- COLLECTION/src/app/Calendar/calendarDao.js
+++ COLLECTION/src/app/Calendar/calendarDao.js
@@ -189,10 +189,10 @@ async function selectWeeklyPlaceName3(connection, userIdx, placeJoinTable) {
 
 async function selectWeeklyOotdList(connection, userIdx){
   const selectWeeklyOotdListQuery = `
-                  SELECT O.ootdIdx, O.date, O.lookpoint, TMPH.imageUrl, group_concat( distinct TMPL.fpName) AS pl1, group_concat( distinct TMPL.apName) AS pl2,
-                  group_concat( distinct TMWE.fwName) AS we1, group_concat( distinct TMWE.awName) AS we2,
-                  group_concat( distinct TMWH.fwhName) AS wh1, group_concat( distinct TMWH.awhName) AS wh2,
-                  group_concat( distinct TMCL.fixedBig) AS big1, group_concat( distinct TMCL.fixedSmall) AS small1, group_concat( distinct TMCL.addedBig) AS big2, group_concat( distinct TMCL.addedSmall) AS small2
+                  SELECT O.ootdIdx, O.date, O.lookpoint, TMPH.imageUrl, group_concat( distinct TMPL.fpName) AS fPlace, group_concat( distinct TMPL.apName) AS aPlace,
+                  group_concat( distinct TMWE.fwName) AS fWeather, group_concat( distinct TMWE.awName) AS aWeather,
+                  group_concat( distinct TMWH.fwhName) AS fWho, group_concat( distinct TMWH.awhName) AS aWho,
+                  group_concat( distinct TMCL.fixedBig) AS fBig, group_concat( distinct TMCL.fixedSmall) AS fSmall, group_concat( distinct TMCL.addedBig) AS aBig, group_concat( distinct TMCL.addedSmall) AS aSmall
                   
                   FROM OOTD AS O
 
@@ -244,84 +244,7 @@ return weeklyOotdRows;
 }
 
 
-/* *************************** */
-
-async function selectWeekly(connection, userIdx) {
-  const selectWeeklyListQuery = `
-                SELECT 
-                FROM 
-                WHERE ;
-                `;
-  const [weeklyRows] = await connection.query(selectWeeklyListQuery, userIdx);
-  return weeklyRows;
-}
-
-/*
-// userId 회원 조회
-async function selectUserId(connection, userId) {
-  const selectUserIdQuery = `
-                SELECT id, email, nickname 
-                FROM UserInfo 
-                WHERE id = ?;
-                `;
-  const [userRow] = await connection.query(selectUserIdQuery, userId);
-  return userRow;
-}
-
-// 유저 생성
-async function insertUserInfo(connection, insertUserInfoParams) {
-  const insertUserInfoQuery = `
-        INSERT INTO UserInfo(email, password, nickname)
-        VALUES (?, ?, ?);
-    `;
-  const insertUserInfoRow = await connection.query(
-    insertUserInfoQuery,
-    insertUserInfoParams
-  );
-
-  return insertUserInfoRow;
-}
-
-// 패스워드 체크
-async function selectUserPassword(connection, selectUserPasswordParams) {
-  const selectUserPasswordQuery = `
-        SELECT email, nickname, password
-        FROM UserInfo 
-        WHERE email = ? AND password = ?;`;
-  const selectUserPasswordRow = await connection.query(
-      selectUserPasswordQuery,
-      selectUserPasswordParams
-  );
-
-  return selectUserPasswordRow;
-}
-
-// 유저 계정 상태 체크 (jwt 생성 위해 id 값도 가져온다.)
-async function selectUserAccount(connection, email) {
-  const selectUserAccountQuery = `
-        SELECT status, id
-        FROM UserInfo 
-        WHERE email = ?;`;
-  const selectUserAccountRow = await connection.query(
-      selectUserAccountQuery,
-      email
-  );
-  return selectUserAccountRow[0];
-}
-
-async function updateUserInfo(connection, id, nickname) {
-  const updateUserQuery = `
-  UPDATE UserInfo 
-  SET nickname = ?
-  WHERE id = ?;`;
-  const updateUserRow = await connection.query(updateUserQuery, [nickname, id]);
-  return updateUserRow[0];
-}
-
-*/
-
 module.exports = {
   selectMonthly,
-  selectWeeklyOotdList,
-  selectWeekly,
+  selectWeeklyOotdList
 };
