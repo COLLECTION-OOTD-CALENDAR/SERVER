ebcb884466c77db893b780641403e1a0553299cd
seoyeonK <hasunnyha@gmail.com>
2022-02-05 01:23:57 +0900

    220205 Fix :: ootdRoute 내 uri 수정

--- COLLECTION/src/app/OOTD/ootdRoute.js
+++ COLLECTION/src/app/OOTD/ootdRoute.js
@@ -4,6 +4,6 @@ module.exports = function(app){
 
 
     // 1. 사용자 블럭 추가 API (JWT 검증 및 Validation )
-    app.post('/app/ootd/new-block/:userId', jwtMiddleware, ootd.postNewBlock); //?Clothes=?&PWW=?', jwtMiddleware, ootd.postNewBlock);
+    app.post('/app/ootd/new-block/:userIdx', jwtMiddleware, ootd.postNewBlock); //, jwtMiddleware, ootd.postNewBlock);
     
 };
