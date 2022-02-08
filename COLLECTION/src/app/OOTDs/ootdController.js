28790b445e5cb87ebe28b73d2a2873987899fd17
CSH <sayong4424@gmail.com>
2022-02-09 02:34:27 +0900

    220209 Fix :: 'photoIs' 변수 디버깅

--- COLLECTION/src/app/OOTDs/ootdController.js
+++ COLLECTION/src/app/OOTDs/ootdController.js
@@ -96,6 +96,7 @@ exports.registerOotd = async function (req, res) {
     }
 
     // photoIs 값 -1 또는 0인지 체크
+    console.log(typeof photoIs);
     if(photoIs != -1 || photoIs != 0){
         return res.send(errResponse(baseResponse.PHOTOIS_INVALID_VALUE));
     }
