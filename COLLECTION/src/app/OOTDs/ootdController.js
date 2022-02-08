7c8f744ae8044650d4288b2c87c9a976c2dc5b86
CSH <sayong4424@gmail.com>
2022-02-09 02:01:37 +0900

    220209 Fix :: 자료형 변환 추가
    lookname toString 진행

--- COLLECTION/src/app/OOTDs/ootdController.js
+++ COLLECTION/src/app/OOTDs/ootdController.js
@@ -36,7 +36,7 @@ exports.registerOotd = async function (req, res) {
 
     // request body 풀어내기
     const n_date = new Date(date);
-    
+
     /*********************************************** */
     /*****************request error***************** */
     /*********************************************** */
@@ -75,13 +75,13 @@ exports.registerOotd = async function (req, res) {
     }
 
     // lookname에 공백만 입력된 경우
-    var n_lookname = lookname.toString();
+    const n_lookname = lookname.toString();
     if(n_lookname.replace(blankPattern, '') == ""){
         return res.send(errResponse(baseResponse.REGISTER_BLANK_ALL));
     }
 
     // lookname 길이가 27자리를 초과할 때
-    if(lookname.length > 27){
+    if(n_lookname.length > 27){
         return res.send(errResponse(baseResponse.LOOKNAME_LENGTH));
     }
 
@@ -199,8 +199,9 @@ exports.registerOotd = async function (req, res) {
         return res.send(errResponse(baseResponse.LOOKPOINT_INVALID_VALUE));
     }
 
+    const n_comment = comment.toString();
     // COMMENT 길이 체크
-    if(comment.length > 65535){
+    if(n_comment.length > 65535){
         return res.send(errResponse(baseResponse.COMMENT_LENGTH));
     }
 
@@ -273,8 +274,8 @@ exports.registerOotd = async function (req, res) {
         }
     }
 
-    const registerUserOotd = await ootdService.lastRegisterOotd(userIdx, date, lookname, photoIs, image, fClothes, aClothes,
-        fPlace, aPlace, fWeather, aWeather, fWho, aWho, lookpoint, comment);
+    const registerUserOotd = await ootdService.lastRegisterOotd(userIdx, date, n_lookname, photoIs, image, fClothes, aClothes,
+        fPlace, aPlace, fWeather, aWeather, fWho, aWho, lookpoint, n_comment);
     return res.send(response(baseResponse.SUCCESS_LAST_REGISTER, registerUserOotd));
 
 };
