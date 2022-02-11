42931601648139969a9f900f814d5c66b0ca150d
seoyeonK <hasunnyha@gmail.com>
2022-02-12 02:01:32 +0900

    220212 Fix :: ootdRoute 내 const upload 수정

--- COLLECTION/src/app/OOTD/ootdRoute.js
+++ COLLECTION/src/app/OOTD/ootdRoute.js
@@ -1,8 +1,7 @@
 module.exports = function(app){
     const ootd = require('./ootdController');
     const jwtMiddleware = require('../../../config/jwtMiddleware');
-    const multer = require('../../../config/Multer');
-
+    const upload = require('../../../config/Multer');
 
     // 1. 사용자 블럭 추가 API (JWT 검증 및 Validation )
     app.post('/app/ootd/new-block', jwtMiddleware, ootd.postNewBlock); //, jwtMiddleware, ootd.postNewBlock);
