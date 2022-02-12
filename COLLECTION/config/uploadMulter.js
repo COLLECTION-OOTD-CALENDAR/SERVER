ace9beacecfcc5000e8ac836e68104f1734d94f1
seoyeonK <hasunnyha@gmail.com>
2022-02-12 14:38:11 +0900

    220212 Fix :: uploadMulter 내 getImageUrl 구조 변경

--- COLLECTION/config/uploadMulter.js
+++ COLLECTION/config/uploadMulter.js
@@ -1,22 +1,23 @@
 const upload = require('./Multer');
-const {logger} = require("./winston");
 
-
-exports.getImageUrl = async (req, res, next) =>
+exports.getImageUrl = async function (req, res)
 {   
-    try{
-        upload.single('image');    
+    this.one = function(){
+        upload.single('image');
+        two();
+    }   
+    function two(){
         const Img = req.file;
         console.log('uploaded image : ', Img.location);
-        res.send(Img.location);  
-        //return response(baseResponse.SUCCESS_IMAGE_URL, {'image url' : Img.location });  
-    }
-    catch(err){
-        logger.error(`App - getImageUrl uploadmulter error\n: ${err.message}`);
-        return errResponse(baseResponse.SERVER_ERROR);
-    }                
+        res.send(Img.location);      
+    }                  
 };
 
+
+
+
+// @ ootdRoute
+//const upload = require('../../../config/Multer');
 
 // app.post('/app/ootd/s3test', upload.single('image'), function(req, res)
     // {   
