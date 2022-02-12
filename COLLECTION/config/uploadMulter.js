92ebf99594491098676380b5a49148bc0f5cf44a
seoyeonK <hasunnyha@gmail.com>
2022-02-12 13:52:31 +0900

    220212 Fix :: test uploadMulter

--- COLLECTION/config/uploadMulter.js
+++ COLLECTION/config/uploadMulter.js
@@ -1,18 +1,18 @@
-// const upload = require('./Multer');
+const upload = require('./Multer');
 
-// exports.getImageUrl = async (req, res, next) =>
-// {   
-//     try{
-//         upload.single('image');    
-//         const Img = req.file;
-//         console.log('uploaded iamge : ', Img.location);
-//         return response(baseResponse.SUCCESS_IMAGE_URL, {'image url' : Img.location });  
-//     }
-//     catch{
-//         logger.error(`App - getImageUrl uploadmulter error\n: ${err.message}`);
-//         return errResponse(baseResponse.SERVER_ERROR);
-//     }                
-// };
+exports.getImageUrl = async (req, res, next) =>
+{   
+    try{
+        upload.single('image');    
+        const Img = req.file;
+        console.log('uploaded iamge : ', Img.location);
+        return response(baseResponse.SUCCESS_IMAGE_URL, {'image url' : Img.location });  
+    }
+    catch{
+        logger.error(`App - getImageUrl uploadmulter error\n: ${err.message}`);
+        return errResponse(baseResponse.SERVER_ERROR);
+    }                
+};
 
 
 // app.post('/app/ootd/s3test', upload.single('image'), function(req, res)
