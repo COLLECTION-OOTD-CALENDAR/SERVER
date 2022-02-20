de4fae79fa7935b82e80c83c47f73d96de08b1dd
CSH <sayong4424@gmail.com>
2022-02-21 02:25:56 +0900

    220221 Fix :: 'searchRoute' 파일 내 오류 수정

--- COLLECTION/src/app/Search3/searchRoute.js
+++ COLLECTION/src/app/Search3/searchRoute.js
@@ -3,7 +3,7 @@ module.exports = function(app){
     const jwtMiddleware = require('../../../config/jwtMiddleware');
 
     // 1. [PWWC] 검색 초기화면 보여주기 API
-    app.get('/app/search/mainpage/:PWWC', jwtMiddleware, searchMain);
+    app.get('/app/search/mainpage/:PWWC', jwtMiddleware, search.searchMain);
 
     // 2. [PWWC] 매칭 페이지 검색 키워드 제안
     //app.get('/app/search/suggestion/:PWWC', jwtMiddleware, suggestSearchKeyword);
