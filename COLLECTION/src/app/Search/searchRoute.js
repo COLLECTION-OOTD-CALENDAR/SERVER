6fead8a4f5b6278d36e663569fa3f12b9fe66f30
seoyeonK <hasunnyha@gmail.com>
2022-02-28 23:22:35 +0900

    220228 Fix :: searchRoute 내 검색결과 보여주기 uri 수정

--- COLLECTION/src/app/Search/searchRoute.js
+++ COLLECTION/src/app/Search/searchRoute.js
@@ -3,6 +3,6 @@ module.exports = function(app){
     const jwtMiddleware = require('../../../config/jwtMiddleware');
 
     // 1. 검색결과 보여주기 API
-    app.get('/app/search/:PWWWC',jwtMiddleware, search.getSearchResult); 
+    app.get('/app/search/:PWWC',jwtMiddleware, search.getSearchResult); 
 
 };
